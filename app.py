8ef4a099a5aa3e3cebddcd7db630b897e2308348
EmmanuelPerezP <emmanuelperezp@gmail.com>
2016-05-12 00:49:31 -0600

    directions

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ class mainWindow:
     enemies = []
     bullets = []
     objects = []
+    players = []
     sizeX = 0
     sizeY = 0
 
@@ -23,41 +24,46 @@ class mainWindow:
         curses.start_color()
         curses.use_default_colors()
         curses.init_pair(1, 5, -1)
+        curses.init_pair(2, 2, -1)
         self.mainW.nodelay(True)
-    
+        mainWindow.sizeX = self.mainW.getmaxyx()[1]
+        mainWindow.sizeY = self.mainW.getmaxyx()[0]
+
     def mainLoop(self):
-        player1 = player(7, 7)
+        player(mainWindow.sizeY//2, mainWindow.sizeX//2)
         self.ventana = curses.newwin(10, 10, 0, 0)
         self.objects.append(self.ventana)
-        self.objects.append(player1)
         curses.curs_set(0)
         mainWindow.sizeX = self.mainW.getmaxyx()[1]
         self.mainW.box()
         while True:
-            #self.ventana.addstr(0,0,str(self.mainW.getmaxyx()),curses.color_pair(1))
+
+            mainWindow.sizeX = self.mainW.getmaxyx()[1]
+            mainWindow.sizeY = self.mainW.getmaxyx()[0]
+
             self.ventana.addstr(0,0,str(mainWindow.sizeX),curses.color_pair(1))
+            self.ventana.addstr(1,0,str(mainWindow.sizeY),curses.color_pair(1))
             self.ventana.noutrefresh()
-            agregarEnemigos = random.randint(0,70)
-            if agregarEnemigos == 5:
+            agregarEnemigos = random.randint(0,300)
+            if agregarEnemigos == 50:
                 self.addEnemies()
             entrada = self.mainW.getch()
-            player1.move(entrada)
+            self.ventana.addstr(0,5,str(entrada),curses.color_pair(1))
             if entrada == 27:
                 break
-           
-            i = 0
-            while i < len(mainWindow.bullets):
-                mainWindow.bullets[i].updateBullet()
-                i+=1
 
-            i = 0
-            while i < len(mainWindow.enemies):
-                mainWindow.enemies[i].updateEnemy()
-                i+=1
+            for i in mainWindow.players:
+                i.move(entrada) 
+            for i in mainWindow.bullets:
+                i.updateBullet()
+            for i in mainWindow.enemies:
+                i.updateEnemy()
+            for i in mainWindow.players:
+                i.updatePlayer()
                 
             curses.panel.update_panels()
             curses.doupdate()
-            time.sleep(0.02)
+            time.sleep(0.006)
 
     def addEnemies(self):
         newEnemies = random.randint(1,3) 
@@ -69,43 +75,114 @@ class mainWindow:
 
 class player:
 
-    Xsteps = 3
-    Ysteps = 2
+    Xsteps = 1
+    Ysteps = 1
+    speed = 5
+    releaseKeys = [ -11, -10, -18, -19]
 
-    def __init__(self, x=5, y=5):
+    def __init__(self, y, x):
         width = 5
         height = 3
         self.currentX = x
         self.currentY = y
+        self.dirX = 0
+        self.dirY = 0
+        self.stop = True
+
         self.playerW = curses.newwin(height, width, self.currentY, self.currentX)
         self.playerW.addstr(0,0," /^\ <\-/>", curses.color_pair(1))
         self.panelJ = curses.panel.new_panel(self.playerW) 
+        self.speed = 0
+        mainWindow.players.append(self)
+
+
+    def updatePlayer(self):
+        """
+        updates the player on screen, it has a delay so its independent of the 
+        refresh rate
+        """
+
+        if self.speed != player.speed:
+            self.speed += 1
+        elif self.speed == player.speed:
+
+            if self.dirX == -1:
+                self.currentX-=player.Xsteps
+            elif self.dirX == 1:
+                self.currentX+=player.Xsteps
+            elif self.dirX == 0:
+                pass
+            
+            if self.currentX == mainWindow.sizeX-4 or self.currentX == 4:
+                self.dirX = 0
+
+            if self.dirY == -1:
+                self.currentY-=player.Ysteps
+            elif self.dirY == 1:
+                self.currentY+=player.Ysteps
+            elif self.dirY == 0:
+                pass
+            
+            if self.currentY == mainWindow.sizeY -4 or self.currentY == 2:
+                self.dirY = 0
+
+            if self.stop == False:
+                self.dirX = 0
+                self.dirY = 0
+                self.stop = True
+    
+
+            self.panelJ.move(self.currentY, self.currentX)
+            self.speed = 0
+
 
     def move(self, key_code):
-        
+        """
+        alters the state of the player acording to the key_code (key pressed)
+        its independent of updatePlayer 
+        if self.currentX == mainWindow.sizeX-5:
+            if key_code == curses.KEY_LEFT:
+                self.currentX -= player.Xsteps
+        elif self.currentX == 5:
+            if key_code == curses.KEY_RIGHT:
+                self.currentX += player.Xsteps
+        elif self.currentY == 5:
+            if key_code == curses.KEY_DOWN:
+                self.currentY += player.Ysteps 
+        elif self.currentY == mainWindow.sizeY-5:
+            if key_code == curses.KEY_UP:
+                self.currentY -= player.Ysteps  
+        """
+
         if key_code == curses.KEY_LEFT:
-            self.currentX -= player.Xsteps
-            self.panelJ.move(self.currentY, self.currentX)
-
+            if self.currentX == 4:
+                self.dirX = 0
+            else:
+                self.dirX = -1
         elif key_code == curses.KEY_RIGHT:
-            self.currentX += player.Xsteps
-            self.panelJ.move(self.currentY, self.currentX)
-
+            if self.currentX == mainWindow.sizeX-4:
+                self.dirX = 0
+            else:
+                self.dirX = 1
         elif key_code == curses.KEY_DOWN:
-            self.currentY += player.Ysteps
-            self.panelJ.move(self.currentY, self.currentX)
-        
+            if self.currentY == mainWindow.sizeY -4:
+                self.dirY = 0
+            else:
+                self.dirY = 1
         elif key_code == curses.KEY_UP:
-            self.currentY -= player.Ysteps 
-            self.panelJ.move(self.currentY, self.currentX)
-        
+            if self.currentY == 2:
+                self.dirY = 0
+            else:
+                self.dirY = -1
         elif key_code == ord(' '):
             bulletFired = bullet(self.currentY, self.currentX, len(mainWindow.bullets))
+        elif key_code == ord('s'):
+            self.stop = not self.stop
 
 
 class enemy1:
 
-    speed = 3
+    speed = 30
 
     def __init__(self, startingY, startingX):
 
@@ -153,7 +230,7 @@ class enemy1:
 
 class bullet:
     
-    bulletSpeed = 1
+    bulletSpeed = 10
         
     def __init__(self, startingY, startingX, index, width=1, height=2):
         self.currentX = startingX +2
@@ -162,7 +239,7 @@ class bullet:
         self.width = width 
         self.height = height 
         self.bulletW = curses.newwin(height, width, self.currentY, self.currentX) 
-        self.bulletW.addstr('|')
+        self.bulletW.addstr('|', curses.color_pair(2))
         #self.bulletW.box()
         self.bulletP = curses.panel.new_panel(self.bulletW) 
         self.speed = 0 
